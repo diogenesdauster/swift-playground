080827a14a44a1162e0b2ae1756d913c14aa5366
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-01-31 21:17:51 -0300

    feat: new playground page about comments

---
+++ MyPlayground.playground/Pages/comments.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// this is a comment. it is not executed
+// another comment.
+// Yet another comment
+
+/*
+ This is a comment spread out over
+ a few lines also, a haiku!
+ */
+
+var str = "Hello, playground"
+
+//: [Next](@next)
--- MyPlayground.playground/Pages/introducion.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/introducion.xcplaygroundpage/Contents.swift
@@ -2,6 +2,21 @@
 
 import Foundation
 
-var str = "Hello, playground"
+var string = "Hello, playground"
+string = "Hi!"
+
+var currenValue = 50
+var targetValue = 55
+
+var difference = currenValue - targetValue
+
+if difference < 0 {
+    difference *= -1
+}
+
+print(difference)
+
+
+
 
 //: [Next](@next)
