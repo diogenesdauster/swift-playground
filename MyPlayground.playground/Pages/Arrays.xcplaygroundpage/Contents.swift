f088a41759862147131159191bb688e218c3a78d
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-02-07 22:32:34 -0300

    feat: add arrays swift's page

---
+++ MyPlayground.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+//: [Previous](@previous)
+
+//let pastries: [String] = ["cookie","cupcake","conout","pie"]
+
+var pastries: [String] = []
+pastries.append("cookie")
+pastries += ["cupcake","donout","pie"]
+
+//pastries.removeAll()
+pastries.isEmpty
+
+pastries.count
+if let first = pastries.first,
+    let min = pastries.min(),
+    let max = pastries.max() {
+    print(first, min, max)
+}
+
+let firstElement = pastries[0]
+let firstThree = Array(pastries[1...3])
+
+
+pastries.contains("donut")
+pastries.contains("lasagna")
+
+pastries.insert("tart", at: 0)
+
+let removedTwo = pastries.remove(at: 2)
+let removedLast = pastries.removeLast()
+
+pastries [0...1] = ["brownie","fritter","tart"]
+
+pastries.swapAt(1, 2)
+
+for (index,pastry) in pastries.enumerated() {
+    print(index,pastry)
+}
+//: [Next](@next)
