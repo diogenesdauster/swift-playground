ad7c104f318a237674ce8d7b18290b9465e10e81
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-02-16 15:59:36 -0300

    refactor:change the example to class instead struc

--- MyPlayground.playground/Pages/Classes vs Structures.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Classes vs Structures.xcplaygroundpage/Contents.swift
@@ -1,25 +1,55 @@
 //: [Previous](@previous)
 
-struct Actor {
+//struct Actor {
+//    let name: String
+//    var filmography: [String] = []
+//
+//    mutating func signOnForSequel(franchiseName: String){
+//        filmography.append("Upcoming \(franchiseName) sequel")
+//    }
+//}
+//
+//var gotgStar = Actor(name: "Zoe Saldana", filmography: ["Guardians of Galaxy"])
+//
+//gotgStar.filmography.append("Avatar")
+//
+//var startTrekStar = gotgStar
+//startTrekStar.filmography.append("Star Trek")
+//
+//var avatarStar = startTrekStar
+//
+//for franchiseName in avatarStar.filmography {
+//    avatarStar.signOnForSequel(franchiseName: franchiseName)
+//}
+
+class Actor {
     let name: String
     var filmography: [String] = []
     
-    mutating func signOnForSequel(franchiseName: String){
+    init(name: String, filmography: [String]) {
+        self.name = name
+        self.filmography = filmography
+    }
+    
+    func signOnForSequel(franchiseName: String){
         filmography.append("Upcoming \(franchiseName) sequel")
     }
 }
 
-var gotgStar = Actor(name: "Zoe Saldana", filmography: ["Guardians of Galaxy"])
+let gotgStar = Actor(name: "Zoe Saldana", filmography: ["Guardians of Galaxy"])
 
 gotgStar.filmography.append("Avatar")
 
-var startTrekStar = gotgStar
+let startTrekStar = gotgStar
 startTrekStar.filmography.append("Star Trek")
 
-var avatarStar = startTrekStar
+let avatarStar = startTrekStar
 
 for franchiseName in avatarStar.filmography {
     avatarStar.signOnForSequel(franchiseName: franchiseName)
 }
 
+startTrekStar
+avatarStar
+gotgStar
 //: [Next](@next)
